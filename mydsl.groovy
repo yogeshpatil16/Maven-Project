5dbb900076cac84920527cf355e20084e49a7405
Gagandeep Singh <admin.gagan@gmail.com>
2022-01-07 06:08:03 +0530

    Create mydsl.groovy

---
+++ mydsl.groovy
@@ -0,0 +1,13 @@
+job('my-dsl-job-1') {
+    logRotator(2, 10)
+    jdk('Java 8')
+    scm {
+        git('https://github.com/admingagan/Maven-Project.git', 'master')
+    }
+    triggers {
+        githubPush()
+    }
+    steps {
+        maven ('clean package')
+    }
+}
